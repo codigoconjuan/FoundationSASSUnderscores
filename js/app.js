e3a6f1a01c49f7c6775a1d2153fe36b680715b2f
JuanDevWP <blackmaxxgdl18@hotmail.com>
2016-08-22 10:46:25 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$ = jQuery.noConflict();
+
+$(document).foundation();
+
+$(document).ready(function() {
+
+  var options = {
+      animInFromLeft : 'fade-in',
+      animInFromRight : 'fade-in',
+      animOutToLeft: 'fade-out',
+      animOutToRight : 'fade-out'
+  };
+
+  var element = $('.orbit');
+
+  var slider = new Foundation.Orbit(element, options);
+
+});
